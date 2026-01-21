87b74e316beaff558869cb4e238c021371623884
Divya Chadar <divyachadar5@gmail.com>
2026-01-21 19:58:50 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+// class main {
+//     public static void main(String[] args) {
+//         int arr[]={1,2,3,5,5,6};
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         map.put(1,10);
+//         map.put(2,20);
+//         System.out.println(map.get(3));
+//         System.out.println(map.getDefault(3.2));
+//         System.out.println(map.containKey(3));
+
+//         for(int x :map.KeySet()){
+//             System.out.println(map.get(x));
+
+//         }
+//     }
+// }
+
+class main{
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int arr[]=
+    }
+}
+
+
+
+
---
+++ sum.java
@@ -0,0 +1,11 @@
+class Main{
+public static void main(String[] args){
+    int arr[]={2,7,11,15};
+    int sum=0;
+    for(int i=0;i<2;i++){
+        sum = sum+arr[i];
+    }
+   System.out.println(sum);
+    
+}
+}
